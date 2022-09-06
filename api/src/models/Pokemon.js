a0631221ac83e9d8e1008a6fa96df9978dfb185b
Tu usuario de GitHub aca <Tu email aca>
2022-09-05 23:57:18 -0300

    chau db

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,82 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      //UUID = Universally Unique Identifier
+      ID: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false, //ver si es necesario
+        primaryKey: true,
+      },
+      Name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      Image: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      Hp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Speed: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          isInt: true,
+        },
+      },
+      Create: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
